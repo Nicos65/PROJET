9ccbdddee631624c1c962d2eea45baf4546348a0
Musa-f <musaevafatima77@gmail.com>
2022-10-28 10:45:14 +0200

    carrousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,42 +1,52 @@
 const slide1 = document.querySelector("#slide1");
 const slide2 = document.querySelector("#slide2");
 const slide3 = document.querySelector("#slide3");
-const slide4 = document.querySelector("#slide4");
-const slide5 = document.querySelector("#slide5");
-const slides = [slide1, slide2, slide3, slide4, slide5];
+const slides = [slide1, slide2, slide3];
 
 let currentSlideIndex = 0;
-const next = document.querySelector("#right");
+const next = document.querySelector('#right');
 const previous = document.querySelector("#left");
 
-const printSlide = (target) => {
-  for(i=0; i<slides.length; i++) {
-    slides[i].classList.remove("slideActive");
-  }
-  target.classList.add("slideActive"); // classe display actif
+
+function printSlide(listeIndex) {
+    for(i=0; i<slides.length; i++) {
+        slides[i].classList.remove("slideActive");
+    }
+    listeIndex.classList.add("slideActive");
 }
 
-printSlide(slides[currentSlideIndex]);
+printSlide(slides[currentSlideIndex]); 
 
-const handleNext = () => {
-  if(currentSlideIndex === slides.length - 1) {
-    currentSlideIndex = 0;
-  }
-  else {
-    currentSlideIndex += 1;
-  }
-  printSlide(slides[currentSlideIndex]);
+
+function handleNext(){
+    if(currentSlideIndex === slides.length - 1) {
+        currentSlideIndex = 0;
+    }
+    else {
+        currentSlideIndex += 1;
+    }
+    printSlide(slides[currentSlideIndex]);
 }
 
-const handlePrevious = () => {
-  if(currentSlideIndex === 0) {
-    currentSlideIndex = slides.length - 1;
-  }
-  else {
-    currentSlideIndex -= 1;
-  }
-  printSlide(slides[currentSlideIndex]);
+function handlePrevious(){
+    if(currentSlideIndex === 0) {
+        currentSlideIndex = slides.length - 1;
+    }
+    else {
+        currentSlideIndex -= 1;
+    }
+    printSlide(slides[currentSlideIndex]);
 }
 
-next.addEventListener("click", handleNext);
-previous.addEventListener("click", handlePrevious);
+
+//Appel fonction HandleNext et HandlePrevious
+next.addEventListener('click',function ()
+{
+    handleNext();
+}  );
+
+previous.addEventListener('click',function ()
+{
+    handlePrevious();
+}  );
+
